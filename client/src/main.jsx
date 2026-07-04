e9fc640ab608ff4b7c1e501b51c4e777cb00eeaf
Abdull-Majeed <majeedpalli46@gmail.com>
2026-07-04 15:30:03 +0500

    Fix ngrok browser warning crash

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
+import axios from 'axios'
+
+axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
